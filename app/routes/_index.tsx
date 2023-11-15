e274653b4311ddfa97214fb801a57db09a9fb65d
Karl <karlcereno@gmail.com>
2023-11-15 17:15:55 -0500

    Set up basic skeleton

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,34 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { MetaFunction } from '@remix-run/node';
+import { Form } from '@remix-run/react';
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: 'New Remix App' },
+    { name: 'description', content: 'Welcome to Remix!' },
   ];
 };
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
+    <>
+      <header className="text-center py-20">
+        <h1 className="text-7xl">Local Coverage Determinations Scraper</h1>
+      </header>
+      <section>
+        <Form method="POST">
+          <button
+            className="px-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 text-xl"
+            type="submit"
           >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+            Scrape
+          </button>
+        </Form>
+      </section>
+    </>
   );
+}
+
+export async function action() {
+  console.log('action');
+  return null;
 }
